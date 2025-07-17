a8e52e294a9af95080ce888c8332f556d86d671b
Deepak Thiagarajan <thiagarajandeepu2007@gmail.com>
2025-07-17 13:46:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function showSidebar() {
   document.querySelector('.sidebar').classList.add('active');
 }
+
 function hideSidebar() {
   document.querySelector('.sidebar').classList.remove('active');
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   document.querySelector('.sidebar').classList.remove('active');
 });
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
   if (window.innerWidth > 992) {
     document.querySelector('.sidebar').classList.remove('active');
   }
